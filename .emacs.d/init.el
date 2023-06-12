ca242e9cbc6d3f5e1e391a8965c29bf033a284a4
Antti Auranen <antti.auranen@iki.fi>
2023-06-12 08:05:01 +0300

    changes to emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,11 +17,6 @@
 (require 'use-package)
 (setq use-package-always-ensure 't)
 
-;; Set up package.el to work with MELPA
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-(package-refresh-contents)
 
 ;; Download Evil
 (unless (package-installed-p 'evil)
@@ -30,18 +25,6 @@
 ;; Enable Evil
 (require 'evil)
 (evil-mode 1)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil use-package undo-tree)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 ;; Visual bell
 (setq visible-bell t)
@@ -51,7 +34,20 @@
   (package-install 'highlight-indent-guides))
 (add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
 
-(highlight-indent-guides-method 'character)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(highlight-indent-guides-method 'character)
+ '(package-selected-packages '(evil use-package undo-tree)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil)))))
 (use-package monokai-pro-theme
   :ensure t
   :config
@@ -61,10 +57,8 @@
 (global-display-line-numbers-mode)
 (global-hl-line-mode 1)
 
-(require 'popwin)
-(popwin-mode 1)
-
 ;; (push '(direx:direx-mode :position left :width 25 :dedicated t)
 ;;       popwin:special-display-config)
 ;; (global-set-key (kbd "C-x C-j") 'direx:jump-to-directory-other-window)
-(require 'neotree)
+
+(right-click-context-mode 1)
